853abeb4ccf3d0732ca50f2d7725f6c93f4e6399
Paulo <paulo.feresin@ringa.com.br>
2023-08-20 19:33:00 -0300

    2.5 segundos

--- test3.js
+++ test3.js
@@ -1,58 +1,52 @@
-function test1() {
-  let smallGear = 300;
-  let bigGear = 700;
+async function test1() {
+  const smallGear = 3000;
+  const bigGear = 7000;
+  const lastGear = smallGear * bigGear;
 
-  let allSmall = [];
-  let allBig = [];
-  let possibleGears = [];
-  const impossibleGears = [];
+  const possibleGears = new Set();
 
-  async function small() {
-    for (let i = 1; i <= smallGear; i++) {
-      allSmall.push(i);
+  /* Achamos todas as marchas possivel e adicionamos no Set possibleGears, que já elimina valores duplicados*/
+  for (let i = 1; i <= smallGear; i++) {
+    for (let j = 1; j <= bigGear; j++) {
+      possibleGears.add(i * j);
     }
   }
 
-  async function big() {
-    for (let i = 1; i <= bigGear; i++) {
-      allBig.push(i);
-    }
-  }
+  const impossibleGears = lastGear - possibleGears.size;
 
-  async function calc() {
-    for (const element of allBig) {
-      for (const gear of allSmall) {
-        possibleGears.push(element * gear);
-      }
-    }
-  }
-  small();
-  big();
-  calc();
+  /* Percorremos todos os numeros até a marcha máxima e pushamos no array impossibleGears as marchas não encontradas */
 
-  const lastGear = possibleGears.slice(-1);
-  const gears = new Set(possibleGears);
+  // for (let i = 1; i <= lastGear; i++) {
+  //   if (!possibleGears.has(i)) {
+  //     impossibleGears.push(i);
+  //   }
+  // }
 
-  async function checkImpossibleGears() {
-    /* Percorremos todos os numeros até a marcha máxima e pushamos no array impossibleGears as marchas não encontradas */
-    for (let i = 1; i <= lastGear; i++) {
-      if (!gears.has(i)) {
-        impossibleGears.push(i);
-      }
-    }
-  }
-  checkImpossibleGears();
   /*LOGS DE VERIFICAÇÃO*/
 
-  console.log("marcha máxima = " + lastGear);
-  console.log("possiveis = " + gears.size);
-  console.log("Impossiveis = " + impossibleGears.length);
-  console.log("somatoria = " + (gears.size + impossibleGears.length)); // É a soma das marchas possiveis e impossiveis, portanto precisa ser igual ao valor da ultima marcha
-  return impossibleGears.length;
+  // console.log("marcha máxima = " + lastGear);
+  // console.log("possiveis = " + possibleGears.size);
+  // console.log("Impossiveis = " + impossibleGears.length);
+  // console.log("somatoria = " + (possibleGears.size + impossibleGears.length)); // É a soma das marchas possiveis e impossiveis, portanto precisa ser igual ao valor da ultima marcha
+  return impossibleGears;
 }
 
-// const start = performance.now();
-// test1();
-// const end = performance.now();
-// const elapsed = end - start;
-// console.log("Tempo de execução: " + elapsed);
+// ------------------------------------
+
+const start = performance.now();
+test1().then((result) => {
+  const end = performance.now();
+  const elapsed = end - start;
+  console.log("Número de marchas impossíveis:", result);
+  console.log("Tempo de execução:", elapsed, "ms");
+});
+
+/* 
+marcha máxima = 21000000
+possiveis = 6218446
+Impossiveis = 14781554
+somatoria = 21000000
+Tempo de execução: 13662.518999993801
+*/
+
+// ------------------------------------
