6690f9630a19a311bdc24e7821d6e3ac2e9a1663
˜PauloFeresin˜ <paulo.feresin@gmail.com>
2023-08-21 08:19:34 -0300

    final

--- getGears.js
+++ getGears.js
@@ -1,11 +1,11 @@
-async function test1() {
+async function getGears() {
   const smallGear = 3000;
   const bigGear = 7000;
   const lastGear = smallGear * bigGear;
 
   const possibleGears = new Set();
 
-  /* Achamos todas as marchas possivel e adicionamos no Set possibleGears, que já elimina valores duplicados*/
+  /* Achamos todas as marchas possiveis e adicionamos no Set possibleGears, que já elimina valores duplicados*/
   for (let i = 1; i <= smallGear; i++) {
     for (let j = i; j <= bigGear; j++) {
       possibleGears.add(i * j);
@@ -20,19 +20,9 @@ async function test1() {
 // ------------------------------------
 
 const start = performance.now();
-test1().then((result) => {
+getGears().then((result) => {
   const end = performance.now();
   const elapsed = end - start;
   console.log("Número de marchas impossíveis:", result);
   console.log("Tempo de execução:", elapsed, "ms");
 });
-
-/* 
-marcha máxima = 21000000
-possiveis = 6218446
-Impossiveis = 14781554
-somatoria = 21000000
-Tempo de execução: 13662.518999993801
-*/
-
-// ------------------------------------
